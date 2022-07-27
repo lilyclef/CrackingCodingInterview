25347d31e4c96fff0590c2950f3a144e76d28849
Yuri Yamamoto <lilyclef@gmail.com>
2022-07-27 14:00:52 +0900

    4.7 topological sort

--- Chapter04_Tree_Graph/04-07_topological_sort/04-07_topological_sort_dfs.cpp
+++ Chapter04_Tree_Graph/04-07_topological_sort/04-07_topological_sort_dfs.cpp
@@ -1,29 +1,57 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node(int v) : val(v) {};
-};
-
-
-Node* createTree(vector<int>& input, int begin, int end) {
-    if (begin > end) return nullptr;
-    int mid = (begin + end) / 2;
-    Node* node = new Node(input[mid]);
-    Node* left = createTree(input, begin, mid - 1);
-    Node* right = createTree(input, mid + 1, end);
-    node->left = left;
-    node->right = right;
-    return node;
+int topological_dfs(unordered_map<int, vector<int>>& mp, vector<int>& ans, int index, vector<int>& reached, vector<int>& executing) {
+  if (reached[index]) return 0;
+  if (executing[index]) return -1;
+  executing[index] = 1;
+  for(int child : mp[index]) {
+    int child_res = topological_dfs(mp, ans, child, reached, executing);
+    if (child_res == -1) return -1;
+  }
+  ans.push_back(index);
+  executing[index] = 0;
+  reached[index] = 1;
+  return 0;
 }
 
 int main () {
-    vector<int> input = {1, 4, 7, 9, 10, 11, 12, 20, 50, 100};
-    Node* head = createTree(input, 0, input.size() - 1);
+  int n = 9;
+  vector<vector<int>> input = {{7, 1}, {7, 8}, {5, 6}, {3, 8}, {3, 0}, {1, 2}, {4, 5}};
+  vector<vector<int>> input_col = {{7, 1}, {7, 8}, {5, 6}, {3, 8}, {3, 0}, {1, 2}, {4, 5}, {2, 1}};
+  unordered_map<int, vector<int>> mp;
+  for (int i = 0; i < input.size(); i++) {
+    mp[input[i][0]].push_back(input[i][1]);
+  }
+  vector<int> ans;
+  vector<int> reached(n, 0);
+  vector<int> executing(n, 0);
+  for (int i = 0; i < n; i++) {
+    int tmp_ans = topological_dfs(mp, ans, i, reached, executing);
+    if (tmp_ans == -1) cout << "collision" << endl;
+  }
+  for (int i = ans.size() - 1; i >= 0; i--) {
+    cout << ans[i] << " ";
+  }
+  cout << endl;
+
+  unordered_map<int, vector<int>> mp_col;
+  for (int i = 0; i < input_col.size(); i++) {
+    mp_col[input_col[i][0]].push_back(input_col[i][1]);
+  }
+
+  ans.clear();
+  reached = vector<int>(n, 0);
+  executing = vector<int> (n, 0);
+  for (int i = 0; i < n; i++) {
+    int tmp_ans = topological_dfs(mp_col, ans, i, reached, executing);
+    if (tmp_ans == -1) cout << "collision! I can't order them" << endl;
+  }
+
+  for (int i = ans.size() - 1; i >= 0; i--) {
+    cout << ans[i] << " ";
+  }
 }
---
+++ Chapter04_Tree_Graph/04-07_topological_sort/04-07_topological_sort_stack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+#include <tuple>
+
+using namespace std;
+
+int topological_stack(vector<vector<int>>& input, vector<int>& ans, int n, vector<int>& reached, vector<int>& executing) {
+  unordered_map<int, vector<int>> mp;
+  for (int i = 0; i < input.size(); i++) {
+    mp[input[i][0]].push_back(input[i][1]);
+  }
+
+  stack<pair<int, int>> tasks;
+  for (int i = 0; i < n; i++) {
+    tasks.push({i, 0});
+  }
+  while (!tasks.empty()) {
+    auto top = tasks.top();
+    int cnode = top.first;
+    int kind = top.second;
+    tasks.pop();
+    if (reached[cnode]) continue;
+    if (kind == 0) {
+      if (executing[cnode]) {
+        cout << "collision" << endl;
+        continue;
+      }
+      executing[cnode] = 1;
+      tasks.push({cnode, 1});
+      for (int child : mp[cnode]) {
+        tasks.push({child, 0});
+      }
+    } else {
+      ans.push_back(cnode);
+      executing[cnode] = 0;
+      reached[cnode] = 1;
+    }
+  }
+
+  return 0;
+}
+
+int main () {
+  int n = 9;
+  vector<vector<int>> input = {{7, 1}, {7, 8}, {5, 6}, {3, 8}, {3, 0}, {1, 2}, {4, 5}};
+  vector<vector<int>> input_col = {{7, 1}, {7, 8}, {5, 6}, {3, 8}, {3, 0}, {1, 2}, {4, 5}, {2, 1}};
+  vector<int> ans;
+  vector<int> reached(n, 0);
+  vector<int> executing(n, 0);
+  topological_stack(input, ans, n, reached, executing);
+  for (int i = ans.size() - 1; i >= 0; i--) {
+    cout << ans[i] << " ";
+  }
+}
